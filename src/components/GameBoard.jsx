707c8a7511489dbdf806904d785368e970aa6cb4
Marta Paz <pazmarta@gmail.com>
2025-04-16 17:05:12 +0100

    update array  set board state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,15 +7,27 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelectBox(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard)  => {
+             // Update the game board with the player's move
+            // contains the old array elements as children elements (the previous state) and the nested arrays copied as well
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X'
+            return updatedBoard
+        })
+    }
+    
     // this board is updated dynamically
     return(
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol className="game-row">
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSelectBox(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
