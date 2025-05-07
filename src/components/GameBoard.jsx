2d804b7762451fbd3b78486385c4c645271c24ab
Marta Paz <pazmarta@gmail.com>
2025-05-07 10:50:12 +0100

    add computed values & remove state
    Prefer Computed values and avoid unnecessary sate management

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,30 +6,30 @@ const initialGameBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard( {onSelectSquare, activePlayerSymbol} ) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+export default function GameBoard( { onSelectSquare } ) {
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
-    function handleSelectBox(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard)  => {
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard)  => {
              // Update the game board with the player's move
             // contains the old array elements as children elements (the previous state) and the nested arrays copied as well
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol
-            return updatedBoard
-        })
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+    //         return updatedBoard
+    //     })
 
-        onSelectSquare()
-    }
+    //     onSelectSquare()
+    // }
     
     // this board is updated dynamically
     return(
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
-                    <ol className="game-row">
+                    <ol className="">
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectBox(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={(onSelectSquare)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
