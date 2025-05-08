99b75e20bd4287713332331a26a8371701add669
Marta Paz <pazmarta@gmail.com>
2025-05-08 19:11:24 +0100

    add helper functions for deriving state
    - Cleaning up App logic
    - Outsourcing into functions gameTurns state
    - Renaming general constants for this app (upper_case style)

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import Player from "./components/Player.jsx"
 import Log from "./components/Log.jsx"
 import GameOver from "./components/GameOver.jsx"
 import { WINNING_COMBINATIONS } from "./winning-combinations.js"
-import { use } from "react"
 
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',  
+}
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
 
 function deriveActivePlayer(gameTurns) {
-  // Check if there are any turns
   let currentPlayer = 'X'
 
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -19,27 +27,9 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer
 }
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-]
+function derivegameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]
 
-
-function App() {
-  const [players, setPlayers]= useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  })
-  const [gameTurns, setGameTurns] = useState([])
-  // const [activePlayer, setActivePlayer] = useState('X')
-
-  const activePlayer = deriveActivePlayer(gameTurns)
-
-  // copy inner arrays so make sure we add a brand new array and not the original gameboard (initial array in memory)
-  let gameBoard = [...initialGameBoard.map(array => [...array])]
-
-  // here deriving the gameboard from gameTurns state
   for (const turn of gameTurns) {
     const { square, player } = turn
     const { row, col } = square 
@@ -47,8 +37,12 @@ function App() {
     gameBoard[row][col] = player
   }
 
+  return gameBoard
+}
+
+function deriveWinner(gameBoard, players) {
   let winner
-  // extract the symbols and positions from the gameboard
+  
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
@@ -56,12 +50,21 @@ function App() {
     
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
       winner = players[firstSquareSymbol]
-      break
     }
   }
 
-  const hasDraw = gameTurns.length === 9 && !winner
+  return winner
+}
 
+function App() {
+  const [players, setPlayers]= useState(PLAYERS)
+  const [gameTurns, setGameTurns] = useState([])
+  
+  const activePlayer = deriveActivePlayer(gameTurns)
+  const gameBoard = derivegameBoard(gameTurns)
+
+  const winner = deriveWinner(gameBoard, players)
+  const hasDraw = gameTurns.length === 9 && !winner
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
@@ -72,9 +75,7 @@ function App() {
       { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
       ]
-      // Log the current player and the selected square
-      // console.log(`Player ${currentPlayer} selected square ${rowIndex},${colIndex}`)
-      // Log the updated game turns
+
       return updatedTurns
     })
   }
@@ -95,8 +96,8 @@ function App() {
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handleNameChange} />
-        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handleNameChange} />
+        <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === 'X'} onChangeName={handleNameChange} />
+        <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer === 'O'} onChangeName={handleNameChange} />
       </ol>
       {(winner || hasDraw) && (<GameOver winner={winner} onRestart={handleRestart} />)}
       {/* set a board prop which gets this gameboard that I'm deriving here now */}
